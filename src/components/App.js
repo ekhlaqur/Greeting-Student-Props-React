c97178a9e93783a17a7353610a83497e36c0046a
Ekhlaqur Rahman <ekhlaqur.rahman91@gmail.com>
2022-12-15 13:41:51 +0530

    content added in src/app.js

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../styles/App.css';
+import Welcome from './Welcome';
+
+const App = () => {
+  return (
+    <Welcome name="Ekhlaqur Rahman" />
+  )
+}
+
+
+export default App;
---
+++ src/components/Welcome.js
@@ -0,0 +1,11 @@
+//  write code for Welcome component here
+import React from "react";
+function Welcome(props){
+    return (
+        <div>
+        <h1>Hey {props.name} !</h1>
+        <h2>Welcome to 10x Academy</h2>
+        </div>
+    )
+}
+export default Welcome;
